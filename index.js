1639eed27c708a94d62dab6c357f137e106b51a7
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2021-02-15 10:45:09 -0500

    updated config

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
 let HC_INIT = false;
 
 const setopts = (opts) => {
-    const defaults = {
-        writeKey: 'null',
-        serviceName: '',
-        dataset: 'test',
-        sampleRate: 10,
-        headerContext: [],
-        errorMessageContext: 'error_message',
-    };
+  const defaults = {
+    writeKey: 'null',
+    serviceName: '',
+    dataset: 'test',
+    sampleRate: 10,
+    headerContext: [],
+    errorMessageContext: 'error_message',
+  };
 
-    const options = { ...defaults, ...opts };
-    return options;
+  const options = { ...defaults, ...opts };
+  return options;
 };
 module.exports = (opts) => ({
-    before: (handler, next) => {
-        const options = setopts(opts);
+  before: (handler, next) => {
+    const options = setopts(opts);
 
-        if (!HC_INIT) {
-            if (options.samplerHook) {
-                // eslint-disable-next-line no-param-reassign, global-require
-                handler.event.beeline = require('honeycomb-beeline')({
-                    writeKey: options.writeKey,
-                    samplerHook: options.samplerHook,
-                    serviceName: options.serviceName,
-                    dataset: options.dataset,
-                });
-            } else {
-                // eslint-disable-next-line no-param-reassign, global-require
-                handler.event.beeline = require('honeycomb-beeline')({
-                    writeKey: options.writeKey,
-                    sampleRate: options.sampleRate,
-                    serviceName: options.serviceName,
-                    dataset: options.dataset,
-                });
-            }
-            HC_INIT = true;
-        } else {
-            // eslint-disable-next-line no-param-reassign, global-require
-            handler.event.beeline = require('honeycomb-beeline')();
-        }
-        // eslint-disable-next-line no-param-reassign
-        handler.event.trace = handler.event.beeline.startTrace();
-
-        options.headerContext.forEach((head) => {
-            const { header, contextname } = head;
-            if (handler.event.headers[header]) {
-                handler.event.beeline.addContext({
-                    [contextname]: handler.event.headers[header],
-                });
-            }
+    if (!HC_INIT) {
+      if (options.samplerHook) {
+        // eslint-disable-next-line no-param-reassign, global-require
+        handler.event.beeline = require('honeycomb-beeline')({
+          writeKey: options.writeKey,
+          samplerHook: options.samplerHook,
+          serviceName: options.serviceName,
+          dataset: options.dataset,
         });
-        return next();
-    },
-    after: (handler, next) => {
-        handler.event.beeline.finishTrace(handler.event.trace);
-        handler.event.beeline.flush();
-        return next();
-    },
-    onError: (handler, next) => {
-        const options = setopts(opts);
-        if (handler.event.beeline) {
-            handler.event.beeline.addContext({
-                error: true,
-                [options.errorMessageContext]: handler.error.toString(),
-            });
-            handler.event.beeline.finishTrace(handler.event.trace);
-            handler.event.beeline.flush();
-        }
+      } else {
+        // eslint-disable-next-line no-param-reassign, global-require
+        handler.event.beeline = require('honeycomb-beeline')({
+          writeKey: options.writeKey,
+          sampleRate: options.sampleRate,
+          serviceName: options.serviceName,
+          dataset: options.dataset,
+        });
+      }
+      HC_INIT = true;
+    } else {
+      // eslint-disable-next-line no-param-reassign, global-require
+      handler.event.beeline = require('honeycomb-beeline')();
+    }
+    // eslint-disable-next-line no-param-reassign
+    handler.event.trace = handler.event.beeline.startTrace();
 
-        return next(handler.error);
-    },
+    options.headerContext.forEach((head) => {
+      const { header, contextname } = head;
+      if (handler.event.headers[header]) {
+        handler.event.beeline.addContext({
+          [contextname]: handler.event.headers[header],
+        });
+      }
+    });
+    return next();
+  },
+  after: (handler, next) => {
+    handler.event.beeline.finishTrace(handler.event.trace);
+    handler.event.beeline.flush();
+    return next();
+  },
+  onError: (handler, next) => {
+    const options = setopts(opts);
+    if (handler.event.beeline) {
+      handler.event.beeline.addContext({
+        error: true,
+        [options.errorMessageContext]: handler.error.toString(),
+      });
+      handler.event.beeline.finishTrace(handler.event.trace);
+      handler.event.beeline.flush();
+    }
+
+    return next(handler.error);
+  },
 });
--- index.test.js
+++ index.test.js
@@ -2,30 +2,28 @@ const middy = require('middy');
 const middyBeeline = require('.');
 
 describe('middy-beeline', () => {
-    it('should extract keys', (done) => {
-        const handler = middy((event, context, callback) => {
-            callback(null, event.extracted);
-        });
-
-        handler.use(
-            middyBeeline({
-                writeKey: 'key',
-                serviceName: 'myService',
-                dataset: 'myDataset',
-            }),
-        );
-
-        const event = {
-            requestContext: {
-                
-            },
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-        handler(event, null, (err) => {
-            expect(err).toBe(null);
-            done();
-        });
+  it('should extract keys', (done) => {
+    const handler = middy((event, context, callback) => {
+      callback(null, event.extracted);
     });
+
+    handler.use(
+      middyBeeline({
+        writeKey: 'key',
+        serviceName: 'myService',
+        dataset: 'myDataset',
+      }),
+    );
+
+    const event = {
+      requestContext: {},
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    handler(event, null, (err) => {
+      expect(err).toBe(null);
+      done();
+    });
+  });
 });
